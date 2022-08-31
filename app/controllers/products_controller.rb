96b3d730d35962a90f8ccf10e8d03ad723cfd04f
Julien Rolland <rollandjulien13013@gmail.com>
2022-08-31 11:06:13 +0200

    move price filter query to /products route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,16 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+
+    if params[:max] && params[:min]
+      @products = Product.where("price > #{params[:min]} and price < #{params[:max]}")
+    elsif params[:max]
+      @products = Product.where("price < #{params[:max]}")
+    elsif params[:min]
+      @products = Product.where("price > #{params[:min]}")
+    else
+      @products = Product.all
+    end
+
 
     render json: @products
   end
@@ -9,15 +19,5 @@ class ProductsController < ApplicationController
     @product = Product.find_by(article_number: params[:id])
 
     render json: @product
-  end
-
-  def price
-    if params[:max] && params[:min]
-      @products = Product.where("price < #{params[:min]} and price < #{params[:max]}")
-    elsif params[:max]
-      @products = Product.where("price < #{params[:max]}")
-    end
-
-    render json: @products
   end
 end

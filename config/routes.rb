243fac293401a75ce8e7c6e012dc16140fda6d10
Julien Rolland <rollandjulien13013@gmail.com>
2022-09-05 13:47:17 +0200

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users,
              controllers: {
-               sessions: 'users/session',
+               sessions: 'users/sessions',
                registrations: 'users/registrations'
              }
   get '/member-data', to: 'members#show'
-  
+
   resources :product_categories, only: %i[index show], path: 'categories', param: :name
   resources :products, only: %i[index show]
 end

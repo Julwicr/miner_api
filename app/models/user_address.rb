df5c95f2a0426a9fe548dea5e9421fa85eeb5aca
Julien Rolland <rollandjulien13013@gmail.com>
2022-09-05 12:10:10 +0200

    removes current user model, session controller and route

--- app/models/user_address.rb
+++
@@ -1,4 +0,0 @@
-class UserAddress < ApplicationRecord
-  belongs_to :user
-  validates :address, :city, :postal_code, :country, :telephone, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  post '/auth', to: 'sessions#auth'
   resources :product_categories, only: %i[index show], path: 'categories', param: :name
   resources :products, only: %i[index show]
 end

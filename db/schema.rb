af75ffbdbb55d292b6b5aacbe75664e9a6141a50
Julien Rolland <rollandjulien13013@gmail.com>
2022-09-05 12:20:24 +0200

    add user model and jwt_denylist to log out user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_132158) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_101337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "product_categories", force: :cascade do |t|
     t.string "name", null: false
@@ -41,30 +49,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_132158) do
     t.index ["product_inventory_id"], name: "index_products_on_product_inventory_id"
   end
 
-  create_table "user_addresses", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "address"
-    t.string "city"
-    t.string "postal_code"
-    t.string "country"
-    t.string "telephone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_addresses_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "password", null: false
-    t.string "email"
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "telephone"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "products", "product_categories"
   add_foreign_key "products", "product_inventories"
-  add_foreign_key "user_addresses", "users"
 end
